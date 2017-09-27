cffb53a14fffb6f8db26d31c7aec589aa807d9ef
user <user@lab327m.tumo.lab>
2017-09-27 17:29:12 +0400

    fdsdsgsdg

--- repofoll.js
+++ repofoll.js
@@ -6,7 +6,7 @@ fs.readFile("usersfinals.json","utf8",function(err,data){
         var i=0;
         
         forEach(users,function(link,index,arr){
-                var url1=link.followers_url;
+                var url1=link.followers_url+"?client_id=1d30df71d656660cd325&client_secret=c84ec9743b7af959db03fe711f6978af0676973d";
                 var array=0;
                 var options={
                      url:url1,
@@ -17,6 +17,7 @@ fs.readFile("usersfinals.json","utf8",function(err,data){
                 ///////////////////
                
                 request(options,function(err,response,json){
+                        if(!err){
                         var file=JSON.parse(json);
                         
                         array=file.length;
@@ -28,6 +29,7 @@ fs.readFile("usersfinals.json","utf8",function(err,data){
                         };
                         var pat=JSON.stringify(app);
                         fs.appendFile("userfollowers.json",pat);
+                        }
                 });
                 
                 //////////////
